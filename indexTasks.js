9efdafff495e3de5d20dc41b3aaa6eb858e2732c
Cast0rM <castor.mfl@gmail.com>
2022-09-20 09:50:49 +0200

    feat: index,update,edit Task

--- indexTasks.js
+++ indexTasks.js
@@ -1,19 +1,69 @@
-function renderTasks(tasks){
+function renderTasks(tasks) {
     const tbody = document.querySelector("#tasks tbody");
+    tbody.replaceChildren([]);
     tasks.forEach((task) => {
         const row = document.createElement('tr');
+
         const idCell = document.createElement('td');
         idCell.innerText = task.id;
+
         const titleCell = document.createElement('td');
         titleCell.innerText = task.title;
+
         const completedCell = document.createElement('td');
         completedCell.innerText = task.completed ? "✔️" : "❌";
+
+        const actionsCell = document.createElement('td');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.setAttribute("id", "deleteTaskButton");
+        deleteButton.innerText = "🗑️";
+        deleteButton.addEventListener("click", (id) => {
+            fetch('http://127.0.0.1:3000/task/' + task.id, {
+                method: "DELETE",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+            }).then(() => {
+                indexTasks();
+            });
+        });
+        actionsCell.appendChild(deleteButton);
+
+        const editButton = document.createElement('button');
+        editButton.setAttribute("id", "editTaskButton");
+        editButton.innerText = "🖊";
+        editButton.addEventListener("click", (id) => {
+            fetch('http://127.0.0.1:3000/task/' + task.id, {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                },
+            }).then((title) => {
+                const updateTask = prompt("UPDATE", task.title);
+                fetch('http://127.0.0.1:3000/tasks', {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json;charset=utf-8'
+                    },
+                    body: JSON.stringify({
+                        id: task.id,
+                        title: updateTask,
+                        completed: task.completed
+                    })
+                })
+                    .then((response) => { return response.json(); })
+                    .then(() => { window.location.reload()})
+                    .catch(() => { alert("Something went wrong"); })
+            });
+        })
+        actionsCell.appendChild(editButton);
 
         row.appendChild(idCell);
         row.appendChild(titleCell);
         row.appendChild(completedCell);
+        row.appendChild(actionsCell)
         tbody.appendChild(row);
-        
     });
 }
 
@@ -23,13 +73,10 @@ function indexTasks() {
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
         },
-
     }).then((response) => {
         return response.json();
-    }).then((data) =>{
-        renderTasks(data);
-    }).catch(() => {
-        alert("Something went wrong")
+    }).then((data) => {
+        renderTasks(data)
     });
 }
 
@@ -37,6 +84,8 @@ document.addEventListener("DOMContentLoaded", () => {
     indexTasks();
 });
 
-function redCreateTask(){
-    window.location.href="./indexCreateTask.html";
-};
+
+
+function redCreateTask() {
+    window.location.href = "./indexCreateTask.html";
+};  
